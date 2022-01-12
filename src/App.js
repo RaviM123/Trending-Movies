98fe4c0c4aa29a5c40541d1a0917a68789a7a9af
Ravi <ravimalakar24@gmail.com>
2022-01-12 20:39:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Movies from './components/Movies';
+import Favourite  from './components/Favourite';
+import {BrowserRouter as Router,Switch,Route,BrowserRouter} from 'react-router-dom'
+function App() {
+  return (
+  
+ <Router>
+  <Navbar/>
+ <Switch>
+ <Route path = '/' exact render ={(props)=>(
+   <>
+   <Banner {...props}/>
+   <Movies {...props}/>
+   </>
+ )}/>
+<Route path='/favourites' component = {Favourite}></Route> 
+
+ </Switch>
+ </Router>
+
+  
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div style = {{display : 'flex'}}>
+          <Link to= "/"  style={{textDecoration: 'none'}}><h1 style = {{marginTop : '1rem', margnLeft: '1rem'}}>Movies App</h1></Link>
+          <Link to= "/favourites"  style={{textDecoration: 'none'}}><h2 style = {{marginLeft : '2rem',marginTop : "1.5rem"}}>Favourites</h2></Link>
+          {/* <h1 style = {{marginTop : '1rem', margnLeft: '1rem'}}>Movies App</h1>
+          <h2 style = {{marginLeft : '2rem',marginTop : "1.5rem"}}>Favourites</h2> */}
+            </div>
+        );
+    }
+}
